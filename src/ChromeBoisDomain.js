8df14798dfb26bf58f2a71779276d1d957f8d287
Alexander Schelchere <ascellie@gmail.com>
2019-08-06 16:09:57 -0400

    aaaaaaaaa

--- src/ChromeBoisDomain.js
+++ src/ChromeBoisDomain.js
@@ -12,8 +12,7 @@ export default class ChromeBoisDomain extends Component {
      * function that has been provided and is already imported
      * (`drawChromeBoiAtCoords` expects two arguments, an x and a y coordinate)
      */
-    const x = event.pageX;
-    const y = event.pageY;
+    const [x, y] = [event.clientX, event.clientY];
     drawChromeBoiAtCoords(x, y);
   };
 
